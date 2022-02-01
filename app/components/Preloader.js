9c7dece842334f4037f9869f97ef9377ee07e029
Lucas Guilherme <lguilherme8@gmail.com>
2022-01-31 21:46:01 -0300

    implemented preloader and home page changes

--- app/components/Preloader.js
+++ app/components/Preloader.js
@@ -1,6 +1,8 @@
 import GSAP from 'gsap'
 import Prefix from 'prefix'
 
+import { each } from 'lodash'
+
 import Component from 'classes/Component'
 
 export default class extends Component {
@@ -11,24 +13,45 @@ export default class extends Component {
       },
       element: '.preloader',
       elements: {
-
+        images: document.querySelectorAll('img'),
+        number: document.querySelector('.preloader__number__text')
       }
     })
+
+    this.createLoader()
+
+    this.length = 0
 
     this.counter = 0
     this.index = 0
 
     this.transformPrefix = Prefix('transform')
+  }
 
-    this.onComplete()
+  onAssetLoaded (image) {
+    this.length += 1
+    const percent = Math.round(this.length / this.elements.images.length * 100)
+
+    this.elements.number.innerHTML = `${percent}%`
+
+    if (percent === 100) {
+      this.onComplete()
+    }
+  }
+
+  createLoader () {
+    each(this.elements.images, element => {
+      element.src = element.getAttribute('data-src')
+      element.onload = () => this.onAssetLoaded(element)
+    })
   }
 
   onComplete () {
     this.timeline = GSAP.timeline()
 
     this.timeline.to(this.element, {
-      autoAlpha: 0,
-      duration: 1
+      y: '-100%',
+      duration: 0.4
     })
 
     this.timeline.call(_ => {
